a6157cfff0ac9816fb65f8c6a9efcdf3f8a66b18
Dmitrii Kotik <dmdoggx@gmail.com>
2026-04-04 13:06:52 +0700

    feat: add model, dataset, and some utils for feature extracting

---
+++ utils/description_features_utils.py
@@ -0,0 +1,40 @@
+def has_bull_markers(desc: str) -> bool:
+    markers_list = ["•", "-", "/"]
+    return any([marker in desc for marker in markers_list])
+
+
+def has_slash(desc: str) -> bool:
+    slashes = ["/", "//"]
+    return any([slash in desc for slash in slashes])
+
+
+def slash_counter(desc: str) -> int:
+    forward_slash_count = desc.count("/")
+    return forward_slash_count
+
+
+def paragraph_counter(desc: str) -> int:
+    paragraphs = desc.count("\n")
+    return paragraphs
+
+
+def word_separately_in_desc(desc: str) -> bool:
+    return "отдельно" in desc.strip().lower()
+
+
+def count_the_occurrence_of_words_for_separation(desc: str) -> int:
+    trigger_words = [
+        "делаем отдельно",
+        "можем отдельно",
+        "отдельную",
+        "самостоятельную",
+        "выполняем отдельно",
+        "при необходимости",
+        "берем отдельно",
+        "также отдельно",
+    ]
+    return sum(desc.lower().count(phrase) for phrase in trigger_words)
+
+def turkney_count(desc: str) -> int:
+    trigger_words = ["комплекс", "не выезжаю", "под ключ"]
+    return sum(desc.lower().count(phrase) for phrase in trigger_words)
